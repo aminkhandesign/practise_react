48b4cf09497ee974a440b6b79c63ae11ae28e8d7
Administrator <administrator@LAUSD-MBP-2017-4.local>
2018-01-18 11:07:21 -0800

    First commit

--- myreact.js
+++ myreact.js
@@ -1,4 +1,11 @@
-ReactDOM.render(
-                React.createElement('h1',{}, "YAYYYY!"),document.getElementById("main"));
+class Mycomp extends React.Component{
+    render(){
+        return <div> <h1>my first component</h1></div>
+    }
+};
+
+ReactDOM.render(<Mycomp />, document.getElementById('main'))
 
 
+
+//we create a react component with React.createClass({render(){}})
